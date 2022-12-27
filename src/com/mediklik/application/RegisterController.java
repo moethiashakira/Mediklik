dd5c77c05168be8400d529f23a614fa0554bbb06
raphael-lesmana <65525536+raphael-lesmana@users.noreply.github.com>
2022-12-27 19:48:30 +0700

    connect scene

--- src/com/mediklik/application/RegisterController.java
+++ src/com/mediklik/application/RegisterController.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import com.mediklik.db.Connect;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -18,7 +19,31 @@ public class RegisterController {
 	private PasswordField passwordPField;
 
 	public void handleRegister() {
+		String userName = usernameTField.getText();
+		String password = passwordPField.getText();
+		Alert registerAlert = new Alert(Alert.AlertType.ERROR);
+		if (!usernameValidation(userName)) {
+			registerAlert.setHeaderText("Invalid username!");
+			registerAlert.showAndWait();
+			return;
+		}
+		if (!passwordValidation(password)) {
+			registerAlert.setHeaderText("Invalid password!");
+			registerAlert.showAndWait();
+			return;
+		}
+		
 		Connect userConnect = Connect.getConnection();
 		PreparedStatement usernamePStatement = userConnect.prepare("insert into user values(?, ?, ?, ?)");
+	}
+	
+	private boolean usernameValidation (String username) {
+		//TODO
+		return username.length() <= 64 && !username.contains(" ");
+	}
+	
+	private boolean passwordValidation (String password) {
+		//TODO
+		return password.length() <= 64;
 	}
 }
